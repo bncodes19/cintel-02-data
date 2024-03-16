dcc479b2e8d77a351a61303e4ba9ef22b592a65d
Brett Neely <154631645+bncodes19@users.noreply.github.com>
2024-03-15 21:28:11 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import plotly.express as px
 from palmerpenguins import load_penguins
 from shiny.express import input, ui, render
-from shinywidgets import render_widget  
+from shinywidgets import render_widget, render_plotly
 import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
@@ -10,38 +10,68 @@ penguins = load_penguins()
 
 ui.page_opts(title="Penguins Dashboard", fillable=True)
 
-ui.input_slider("n", "Number of bins", 1, 100, 20)
-
 with ui.sidebar(position="right", bg="#f8f8f8", open="open"):
-    ui.h2("Sidebar")
+    ui.h2("Interactive Options")
+#  Dropdown input to choose a column
     ui.input_selectize(  
         "select_attribute",  
-        "Name of the selectize",  
-        choices=["Option 1","Option 2","Option 3"],
-        selected=["Green"])
+        "Select column to visualize",  
+        choices=["bill_length_mm","bill_depth_mm","flipper_length_mm", "body_mass_g"],
+        selected=["bill_length_mm"])
+
     ui.input_numeric("plotly_bin_count", "Plotly bin numeric",1,min=1,max=10)
     
     ui.input_slider("seaborn_bin_count", "Seaborn bin count", 10, 100, 20,
                     step=5, animate=True)
     ui.input_checkbox_group(
         "selected_species_list",
-        "Selected species list",
-        choices=["Option 1", "Option 2", "Option 3"],
-        selected="Option 1", inline=True)
+        "Select a species",
+        choices=["Adelie", "Gentoo", "Chinstrap"],
+        selected="Adelie", inline=True)
     ui.hr()
-    ui.h2("Test")
-    ui.a("My GitHub", href="https://github.com/bncodes19/cintel-02-data", target="_blank")
-    # Use ui.hr() to add a horizontal rule to the sidebar
+    ui.h5("GitHub Code Repository")
+    ui.a("cintel-02-data", href="https://github.com/bncodes19/cintel-02-data", target="_blank")
 
+with ui.layout_columns():
+#   a Plotly Histogram
+    with ui.card():
+        ui.card_header("Plotly Histogram")
+        @render_plotly
+        def plotly_histogram():
+            return px.histogram(penguins,x=input.selected_attribute(),nbins=input.plotly_bin_count(),color="species")
 
-#with ui.layout_columns(): to display:
-#   a DataTable (showing all data),
-#   a Data Grid (showing all data), 
+with ui.layout_columns():
+#   a DataTable (showing all data)
+    with ui.card():
+        ui.card_header("Data Table")
+        @render.data_frame
+        def data_table():
+            return render.DataTable(penguins) 
+#   a Data Grid (showing all data)
+    with ui.card():
+        ui.card_header("Data Grid")
+        @render.data_frame
+        def data_grid():
+            return render.DataGrid(penguins)
 
-#with ui.layout_columns():
-fig, ax = plt.subplots()
-@render.plot(alt="A histogram")
-def histogram():
+with ui.layout_columns():
+#    a Plotly Scatterplot (showing all species)
+    with ui.card():
+        ui.card_header("All Species (Plotly Scatterplot)")
+        @render_plotly
+        def plotly_scatterplot():
+            return px.scatter(
+                data_frame=penguins, x="body_mass_g", y="bill_depth_mm",
+                color="species",
+                labels={"bill_depth_mm": "Bill Depth (grams)",
+                    "body_mass_g": "Body Mass (millimeters)"},)
 
 #    a Seaborn Histogram (showing all species)
-#and a Plotly Scatterplot (showing all species)
+    with ui.card():
+        ui.card_header("All Species (Seaborn Histogram)")
+        @render.plot
+        def seaborn_histogram():
+            hist = sns.histplot(data=penguins, x="body_mass_g", bins=input.seaborn_bin_count())  
+            hist.set_xlabel("Mass (grams)")
+            hist.set_ylabel("Count")
+            return hist
